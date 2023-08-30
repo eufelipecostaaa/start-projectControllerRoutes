86c1595120bf4b349fa2cd6e8d4dec354d61660d
eufelipecostaaa <feelingswp9@gmail.com>
2023-08-30 16:43:05 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -99,19 +99,65 @@ Route::get('/aluno/matricula/{id}', function ($id){
     else {
         echo $dados[$id];
     }
-
-    
-
-    /*$cont = 0;
-    $alunos = "<ul>";
-    foreach($dados as $nome){
-        $cont++;
-        if($cont == $id){
-            $alunos = $alunos . "<li>$nome</li>";
-            break;
-        }elseif($cont > $alunos){
-            echo "<h3>Não encontrado!</h3>";
-        }
-    }*/
-
 })->where('total', '[0-9]+');
+
+//4
+Route::get('/aluno/nome/{nome}', function ($nome) {
+    $dados = array(
+        1 => "Carlos Eduardo",
+        2 => "maria Claudia",
+        3 => "Joao Pedro"
+    );
+
+    $nome = strtolower($nome);
+    foreach ($dados as $id => $aluno) {
+        if (strtolower($aluno) === $nome) {
+            return "<li>$id - $aluno</li>";
+        }
+    }
+
+    return "<li>Não encontrado</li>";
+})->where('nome', '[A-Za-z]+');
+
+//5
+Route::get('/aluno/nota', function () {
+    $dados = array(
+        1 => array("nome" => "Carlos Eduardo", "nota" => 8.5),
+        2 => array("nome" => "Maria Claudia", "nota" => 9.0),
+        3 => array("nome" => "Joao Pedro", "nota" => 7.2)
+    );
+
+    $notas = "<ul>";
+    foreach ($dados as $matricula => $aluno) {
+        $notas .= "<li> $matricula {$aluno['nome']} {$aluno['nota']}</li>";
+    }
+    $notas .= "</ul>";
+
+    return "<h1>Matricula Nome Nota</h1>$notas";
+});
+
+//6
+Route::get('/aluno/nota/limite/{quantidade}', function ($quantidade) {
+    $dados = array(
+        1 => array("nome" => "Carlos Eduardo", "nota" => 8.5),
+        2 => array("nome" => "Maria Claudia", "nota" => 9.0),
+        3 => array("nome" => "Joao Pedro", "nota" => 7.2)
+    );
+
+    $notas = "<ul>";
+    $cont = 0;
+    foreach ($dados as $matricula => $aluno) {
+        $notas .= "<li> $matricula {$aluno['nome']} {$aluno['nota']}</li>";
+        $cont++;
+        if ($cont == $quantidade) {
+            break;
+        }
+    }
+    $notas .= "</ul>";
+
+    return "<h1>Matricula Aluno Nota</h1>$notas";
+})->where('quantidade', '[0-9]+')->where('nome', '[A-Za-z]+');
+
+
+
+
